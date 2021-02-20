46d8df9fcf105d5c215db1703ee71a237340be12
Priyanka Singh <priyanka93133@gmail.com>
2021-02-20 14:07:06 +0530

    My First Commit: First Version

---
+++ prog34_Git1.py
@@ -0,0 +1,27 @@
+print("CETPA")
+print("Hello")
+print("Welcome")
+print("ABC")
+
+
+"""
+Mention ur resume Git
+
+Official Topics Left: Next weekend finish
+1. File Handling: Json, Pickling
+2. Database Connectivity
+3. Desktop Application Development: GUI: 1 or 2 Projects
+4. Multithreading
+"""
+
+"Extra Topics"
+"""
+1. Regular Expression
+2. Iterator, Generator, Decorator
+3. Project Voice Recognition
+"""
+
+"Not in Course"
+"Web Application Development: Django/Flask"
+"Web Application Development using Django: 6 Weeks Course"
+
